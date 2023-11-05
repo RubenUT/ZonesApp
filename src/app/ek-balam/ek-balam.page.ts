3dc5589089e44e0ab402156f01c1acea4fd5ecb5
RubenUT <21354385@utcancun.edu.mx>
2023-11-05 16:12:49 -0600

    Implementación de marcadores para el mapa
    Marcadores de la zona arqueologica agregados

--- src/app/ek-balam/ek-balam.page.ts
+++ src/app/ek-balam/ek-balam.page.ts
@@ -1,6 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 declare var google:any;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
+
 @Component({
   selector: 'app-ek-balam',
   templateUrl: './ek-balam.page.html',
@@ -15,7 +24,7 @@ export class EkBalamPage implements OnInit {
     // create a new map by passing HTMLElement
     const mapEle: HTMLElement = document.getElementById('map')!;
     // create LatLng object
-    const myLatLng = {lat: 4.658383846282959, lng: -74.09394073486328};
+    const myLatLng = {lat: 20.891944, lng: -88.135998};
     // create map
     this.map = new google.maps.Map(mapEle, {
       center: myLatLng,
@@ -24,6 +33,22 @@ export class EkBalamPage implements OnInit {
   
     google.maps.event.addListenerOnce(this.map, 'idle', () => {
       mapEle.classList.add('show-map');
+      const marker = {
+        position : {
+          lat: 20.892309,
+          lng: -88.13593
+        },
+        title : 'Zona arqueologica de Ek Balam'
+      }
+      this.addMarker(marker);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
     });
   }
 
