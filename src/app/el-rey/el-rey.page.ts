1fc5f3fd6b683a352ef4a0fecd7c9d814d8257b4
LuisFuen1222 <118927684+LuisFuen1222@users.noreply.github.com>
2023-11-05 18:58:49 -0500

    commit
    SCSS: colores.
    El rey: mapa añadido.

--- src/app/el-rey/el-rey.page.ts
+++ src/app/el-rey/el-rey.page.ts
@@ -1,5 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+declare var google:any;
+
+interface Marker {
+  position: {
+    lat: number,
+    lng: number,
+  };
+  title: string;
+}
 
 @Component({
   selector: 'app-el-rey',
@@ -8,7 +17,40 @@ import { Router } from '@angular/router';
 })
 export class ElReyPage implements OnInit {
 
+  map = null;
   constructor(private router: Router) {}
+
+  loadMap() {
+    // create a new map by passing HTMLElement
+    const mapEle: HTMLElement = document.getElementById('map')!;
+    // create LatLng object
+    const myLatLng = {lat: 21.059251, lng: -86.781508};
+    // create map
+    this.map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+  
+    google.maps.event.addListenerOnce(this.map, 'idle', () => {
+      mapEle.classList.add('show-map');
+      const marker = {
+        position : {
+          lat: 21.059251,
+          lng: -86.781508
+        },
+        title : 'Zona arqueologica de Ek Balam'
+      }
+      this.addMarker(marker);
+    });
+  }
+
+  addMarker(marker: Marker) {
+    return new google.maps.Marker({
+      position: marker.position,
+      map: this.map,
+      title: marker.title
+    });
+  }
 
   goToHome() {
     this.router.navigateByUrl('/home');
@@ -17,6 +59,7 @@ export class ElReyPage implements OnInit {
     window.open('assets/elrey-ar.html');
   }
   ngOnInit() {
+    this.loadMap();
   }
 
 }
